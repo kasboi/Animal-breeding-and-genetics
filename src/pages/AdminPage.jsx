22a19d1c1a4d24db2c82ee8a5478917e68f618a7
xertifiedloaded <certifiedloaded@yahoo.com>
2024-02-18 16:21:53 +0100

    my changes

--- src/pages/AdminPage.jsx
+++ src/pages/AdminPage.jsx
@@ -8,7 +8,7 @@ function Admin() {
   const [totalPages, setTotalPages] = useState(1);
   const [pageSize, setPageSize] = useState(20);
   const [adminEmail, setAdminEmail] = useState("");
-  
+
   useEffect(() => {
     fetchUserInfo();
     fetchUsers();
@@ -20,7 +20,7 @@ function Admin() {
       console.error("Token not found in local storage");
       return;
     }
- 
+
     try {
       const response = await axios.get(process.env.REACT_APP_INFORMATION, {
         headers: {
@@ -33,7 +33,7 @@ function Admin() {
       console.log(error);
     }
   };
-  
+
   const fetchUsers = async () => {
     const token = localStorage.getItem("token");
     if (!token) {
@@ -53,47 +53,51 @@ function Admin() {
       console.log(error);
     }
   };
-  
+
   const handlePageChange = (page) => {
     setCurrentPage(page);
   };
 
   return (
-    <div>
-      <h1>Admin Dashboard</h1>
-      <div>welcome {adminEmail}</div>
-      <table className={styles.table}>
-        <thead>
-          <tr>
-            <th>First Name</th>
-            <th>Last Name</th>
-            <th>Email Address</th>
-            <th>Phone Number</th>
-            <th>Graduation Year</th>
-            <th>Previous Job</th>
-            <th>Current Job</th>
-            <th>Location Or Country</th>
-            <th>Who was your supervisor?</th>
-            <th>Advice for the Department</th>
-          </tr>
-        </thead>
-        <tbody>
-          {users.map((user) => (
-            <tr key={user._id}>
-              <td>{user.firstName}</td>
-              <td>{user.lastName}</td>
-              <td>{user.emailAddress}</td>
-              <td>{user.phoneNumber}</td>
-              <td>{user.graduatedYear}</td>
-              <td>{user.previousJob}</td>
-              <td>{user.currentJob}</td>
-              <td>{user.locationOrCountry}</td>
-              <td>{user.supervisor}</td>
-              <td>{user.advice}</td>
+    <div className={styles.all}>
+      <div className={styles.effect}>
+        <h1>Admin Dashboard</h1>
+        <div>welcome {adminEmail}</div>
+      </div>
+      <div className={styles.tableOverflow}>
+        <table className={styles.table}>
+          <thead>
+            <tr>
+              <th>First Name</th>
+              <th>Last Name</th>
+              <th>Email Address</th>
+              <th>Phone Number</th>
+              <th>Graduation Year</th>
+              <th>Previous Job</th>
+              <th>Current Job</th>
+              <th>Location Or Country</th>
+              <th>Who was your supervisor?</th>
+              <th>Advice for the Department</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {users.map((user) => (
+              <tr key={user._id}>
+                <td>{user.firstName}</td>
+                <td>{user.lastName}</td>
+                <td>{user.emailAddress}</td>
+                <td>{user.phoneNumber}</td>
+                <td>{user.graduatedYear}</td>
+                <td>{user.previousJob}</td>
+                <td>{user.currentJob}</td>
+                <td>{user.locationOrCountry}</td>
+                <td>{user.supervisor}</td>
+                <td>{user.advice}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
       <div className={styles.page}>
         <button
           disabled={currentPage === 1}
@@ -116,13 +120,3 @@ function Admin() {
 }
 
 export default Admin;
-
-
-
-
-
-
-
-
-
-
